dd6f94b8b5ed1939bad36df0c8956bfdbb2bf5aa
Dario OCHOA <dario.martin.8a@gmail.com>
2025-07-19 17:28:53 -0300

    refactor: usar Ticker en movimiento.cpp para no perder eventos del PIR

--- sensores_wemos_D1_mini/movimiento.cpp
+++ sensores_wemos_D1_mini/movimiento.cpp
@@ -3,7 +3,8 @@
 #include <ESP8266WiFi.h> // Por si usás bot->sendMessage
 #include "config_wifi.h" // CHAT_ID
 #include <UniversalTelegramBot.h>
-
+#include <Ticker.h> //para usar las interrupciones por tiempo
+ 
 #define PIR_PIN D6  // GPIO12
 #define RADAR_PIN D7 // GPIO13
 #define BUZZER_PIN D0 // GPIO16
@@ -13,6 +14,10 @@ extern UniversalTelegramBot* bot;
 bool pir_state = false;
 bool radar_state = false;
 
+// Flags de persistencia
+unsigned long pir_until = 0;
+unsigned long radar_until = 0;
+
 unsigned long lastPIRAlertTime = 0;
 unsigned long lastRadarAlertTime = 0;
 unsigned long lastBothAlertTime = 0;
@@ -20,24 +25,47 @@ unsigned long buzzer_until = 0;
 
 const unsigned long ALERT_INTERVAL = 20000; //20 seg
 
+//badera para modo centinela
 bool modoCentinela = true;
+
+// Intervalos
+const unsigned long SENSOR_CHECK_INTERVAL_MS = 500;  // cada cuánto leer
+const unsigned long DETECTION_PERSIST_MS = 10000;    // cuánto tiempo mantener bandera en alto
+
+Ticker tickerMovimiento;
+
+// Función periodica para leer sensores
+void leerSensoresMovimiento() {
+  //logInfo("###---leer sensores.---###");
+  unsigned long now = millis();
+  if (digitalRead(PIR_PIN)) {
+    pir_until = now + DETECTION_PERSIST_MS;
+    logInfo("👤📡 PIR detectado");
+  }
+  if (digitalRead(RADAR_PIN)) {
+    radar_until = now + DETECTION_PERSIST_MS;
+    logInfo("🚶‍♂️📡 RADAR detectado");
+  }
+}
 
 void initMovimiento() {
   pinMode(PIR_PIN, INPUT);
   pinMode(RADAR_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
   digitalWrite(BUZZER_PIN, LOW);
+
+  tickerMovimiento.attach_ms(SENSOR_CHECK_INTERVAL_MS, leerSensoresMovimiento);
+
 }
 
 void updateMovimiento() {
-  pir_state = digitalRead(PIR_PIN);
-  radar_state = digitalRead(RADAR_PIN);
-
   unsigned long now = millis();
+  bool pir = getPIR();
+  bool radar = getRadar();
 
-  if (pir_state && radar_state) {
+  if (pir && radar) {
     buzzer_until = now + 30000;
-  } else if (pir_state || radar_state) {
+  } else if (pir || radar) {
     if (buzzer_until < now) {
       buzzer_until = now + 10000;
     }
@@ -57,13 +85,13 @@ void evaluarAlerta() {
   unsigned long now = millis();
   String msg;
 
-  if (pir_state && radar_state && now - lastBothAlertTime > ALERT_INTERVAL) {
+  if (getPIR() && getRadar() && now - lastBothAlertTime > ALERT_INTERVAL) {
     msg = "🥷 Alerta: PIR y radar detectaron movimiento";
     lastBothAlertTime = now;
-  } else if (pir_state && now - lastPIRAlertTime > ALERT_INTERVAL) {
+  } else if (getPIR() && now - lastPIRAlertTime > ALERT_INTERVAL) {
     msg = "👤 Alerta: PIR detectó movimiento";
     lastPIRAlertTime = now;
-  } else if (radar_state && now - lastRadarAlertTime > ALERT_INTERVAL) {
+  } else if (getRadar() && now - lastRadarAlertTime > ALERT_INTERVAL) {
     msg = "🚶‍♂️ Alerta: Radar detectó movimiento";
     lastRadarAlertTime = now;
   }
@@ -79,11 +107,11 @@ void setModoCentinela(bool valor) {
 }
 
 bool getPIR() {
-  return pir_state;
+  return millis() < pir_until;
 }
 
 bool getRadar() {
-  return radar_state;
+  return millis() < radar_until;
 }
 
 bool getBuzzer() {
